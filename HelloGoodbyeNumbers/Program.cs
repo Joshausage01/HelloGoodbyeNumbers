5d1b5b814b9de51464b4b10d9efe853fe677b303
Joshausage01 <159090739+Joshausage01@users.noreply.github.com>
2024-03-28 19:17:29 +0800

    Initial commit

---
+++ HelloGoodbyeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+NAME:   CRISOSTOMO, JOSHUA B.
+COURSE/SECTION: BSCPE 1-1
+*/
+using System;
+public class HelloGoodbyeNumbers
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                if (number % 3 == 0 && number % 5 != 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (number % 5 == 0 && number % 3 != 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloGoodbyeNumbers
+ Assignment no.1.2 in OOp where the program replace multiples of 3 and 5 with "Hello" and "Goodbye".
